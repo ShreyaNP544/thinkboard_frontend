0cbcf7a1a7a4fe15d1d1d742a563e6724511ce63
ShreyaNP544 <pannaseshreya5n@gmail.com>
2026-04-14 15:25:01 +0530

    build fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI';
 import api from '../libs/axios';
 import toast from 'react-hot-toast';
